615b3dc6eecfbddcd56bf2faa8ab660eec78a00f
Conner DiPaolo <cdipaolo96@gmail.com>
2016-02-25 21:16:01 -0800

    Added hessian, grad, etc. for the f : R^2 -> R

--- plots.py
+++ plots.py
@@ -1,5 +1,8 @@
 import numpy as np
+from numpy.linalg import inv
 from matplotlib import pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib.mlab import griddata
 
 ## make plot of root-finding
 ##
@@ -49,6 +52,54 @@ def plot_root_find():
         plt.ylabel('$y$')
         plt.savefig('plots/root_plt_{}.png'.format(i), bbox_inches='tight')
 
+def f2_mult(x,y):
+    return -np.cos(x*x + y*y) - np.exp(-x*x - y*y)
+
+def f2(x):
+    '''f2 : R^2 -> R'''
+    xx = np.sum(x*x, axis=1)
+    return -np.cos(xx) - np.exp(-xx)
+
+
+def f2grad(x):
+    '''second function gradient'''
+    xx = np.sum(x*x, axis=1)
+    constant = 2*np.exp(-xx) + 2*np.sin(xx)
+    return np.array([x*constant, y*constant])
+
+def f2hessian(x):
+    '''second functon inverse hessian
+    evaluated at x'''
+    xx = np.sum(x*x, axis=1)
+    # compute the hessian
+    A = np.zeros((2,2))
+    A[0,0] = 2*(2*x[0]*x[0]*np.cos(xx) + np.exp(-xx) * (1 - 2*x[0]*x[0] + np.exp(xx) * np.sin(xx)))
+    A[0,1] = 4*np.product(x)*(-np.exp(-xx) + np.cos(xx))
+    A[1,0] = A[0,1]
+    A[1,1] = 2*(2*x[1]*x[1]*np.cos(xx) + np.exp(-xx) * (1 - 2*x[1]*x[1] + np.exp(xx) * np.sin(xx)))
+    return inv(A) # returns the inverse of the hessian
+
+def plot_f2():
+    '''makes a single plot of
+    the function f2 in R^3 for
+    evaluation purposes'''
+    x = np.mgrid[-2:2:0.1,-2:2:0.1].reshape(2,-1).T
+    z = f2(x)
+    X,Y,Z = grid(x[:,0],x[:,1],z)
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.plot_surface(X,Y,Z, color='red', alpha=0.9, label='$f(x) = -\cos(x^2 + y^2) - e^{-(x^2 + y^2)}$')
+    plt.show()
+
+def plot_optimize():
+    '''
+    uses newton's method to optimize
+    a function
+        f : R^n -> R
+    and plot the progression
+    '''
+    root_L = newtons_method_opt(f2grad, f2hessian, 0.4, iters=28)
+
 def makeplots():
     plot_root_find()
 
@@ -65,11 +116,28 @@ def newtons_method(f, fprime, x0, iters=4):
     np array.
 
     outputs:
-      x -> 1d np.array
+      x -> 1xn - d np.array
     '''
     x = [x0]
     for i in range(iters):
         x.append(x[-1] - f(x[-1])/fprime(x[-1]))
+    return x
+
+def newtons_method_opt(gradient, hessian_inv, x0, iters=4):
+    '''newtons_method_opt
+    approximates finding the minimum of f
+    with m in n variables
+    using Newton's Method:
+      x_{n+1} = x_n - H^{-1}\\nabla f(x_n)
+    returns the sequence x_i as an
+    np array.
+
+    outputs:
+      x -> mxn - d np.array
+    '''
+    x = [x0]
+    for i in range(iters):
+        x.append(x[-1] - np.dot(hessian_inv(x[-1]), grad(x[-1])))
     return x
 
 def points(f,start,stop,num):
@@ -94,6 +162,6 @@ def points(f,start,stop,num):
 ############################
 ## Main
 ############################
-
+'''
 if __name__ == '__main__':
-    makeplots()
+    makeplots()'''
